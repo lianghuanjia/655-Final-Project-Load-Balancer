2355646e62ac657526e154f64d2aa2dc13a06fa7
zgong26 <79713887+zgong26@users.noreply.github.com>
2022-12-07 10:28:44 -0500

    Update load_balance.py

--- load_balance.py
+++ load_balance.py
@@ -4,7 +4,8 @@ from websocket_server import WebsocketServer
 from socket import *
 
 # API https://github.com/Pithikos/python-websocket-server
-# 回调
+# Note that list structure is thread-safe in python. Deadlock is not possible in this program
+
 
 # task queue
 tasks = []
@@ -12,59 +13,59 @@ tasks = []
 ips = ["192.122.236.104", "171.67.92.155", "129.110.253.31"]
 
 clients = [None] * 3
-available_machine = [False, False, False]
-dict = {}
+available_machine = [False, False,
+                     False]  # list to lock backend machines and block potential data transferring to the working machines
+dict = {}  # dictionary to store pending messages
 
 
+# This function is called when the load and balance server is receiving message from frontend
 def message_received(client, server, message):
     clientID = client["id"]
-    # 接收到了uri
-    if clientID in dict: #如果client之前有发过来message, 就把这个message加到client 之前message的末端。client 里面应该有几个field，不知道能不能直接用client来当key.
+    # Received URI
+    if clientID in dict:  # If the client hasn't finished receiving messages, append the continued message to the dictionary
         dict[clientID][1] += message
-        if (message.endswith("\n")):#如果这个message是\n结尾，那就说明这个uri传完了，我们有一个完整的uri
+        if (message.endswith("\n")):  # URI ends with '\n' indicates end of message
             print("get full message")
-            tasks.append((dict[clientID][0], dict[clientID][1]))#把这个client和他完整的uri放到tasks里面
-            dict.pop(clientID) #把这个client和他的message从dictionary移除，避免之后这个client发过来，我们把他新的uri和旧的uri连在一起
-    else: #这个uri是新的，之前没有储存过这个client这个uri的数据
-        if(message.endswith("\n")):#如果这个message直接以\n结尾，那我们就是一次把整个完整的uri拿到了，就直接放进去tasks
-            tasks.append((client, message)) #放进去tasks
-        else: #这个uri只是其中一部分，还不完整，我们就把这个uri和对应的client放到dict里面
-            dict[clientID] = [client, message] #放到dict里面
+            tasks.append((dict[clientID][0], dict[clientID][1]))  # append a tuple of client and its completed message
+            dict.pop(clientID)  # remove the finished client from the dictionary
+    else:  # situation when the message is new
+        if (message.endswith("\n")):  # URI ends with '\n' indicates end of message
+            tasks.append((client, message))
+        else:  # otherwise add this new client-message pair to the dictionary
+            dict[clientID] = [client, message]  # store temp message
 
 
-    # 储存到tasks queue里
-    #tasks.append((client, message))
-
-
+# This function is called when load balance is needed to handle multiple thread situation
 def load_balance():
-    # 从queue中取出下一个task交给后端一台available machine上
+    # This function is executed by a thread. Take out a task and send to appropriate backend machines
     while True:
         if True in available_machine and len(tasks) != 0:
             index = available_machine.index(True)
             available_machine[index] = False  # 锁死
             task = tasks.pop(0)
-            client = task[0]  # 获取client
-            message = task[1] + "\n"  # 获取url
-            clients[index] = client  # 储存client信息
-            threading.Thread(target=task_handler, args=(index, message)).start()  # 起线程处理信息
+            client = task[0]  # get client
+            message = task[1] + "\n"  # get url
+            clients[index] = client  # store the client info
+            threading.Thread(target=task_handler,
+                             args=(index, message)).start()  # start a thread to handle the task in a backend machine
 
 
+# load balancing server and backend machine interaction
 def task_handler(index, message):
-    print("available machine: "+str(index))
-
-    s = socket(AF_INET, SOCK_STREAM)
+    print("available machine: " + str(index))
+    s = socket(AF_INET, SOCK_STREAM)  # Establish TCP connection with the backend
     s.connect((ips[index], 12345))
-    s.send(message.encode())  # 发送url
-    res = s.recv(4096)  # 接受结果
+    s.send(message.encode())  # send image as URI
+    res = s.recv(4096)  # receive result from backend
     client = clients[index]
     clients[index] = None
-    server.send_message(client, res.decode())  # 向前端返回结果
-    available_machine[index] = True  # 解锁
-    s.close()
+    server.send_message(client, res.decode())  # send result back to frontend
+    available_machine[index] = True  # unlock the available machine
+    s.close()  # close socket
 
 
 if __name__ == '__main__':
-    #ask what mode
+    # ask what mode
     mode = int(input("1. Machine1\n2. Machine2\n3. Machine3\n4. Auto LoadBalancing\n\n"))
     if mode < 4:
         available_machine[mode - 1] = True
@@ -72,10 +73,9 @@ if __name__ == '__main__':
         available_machine[0] = True
         available_machine[1] = True
         available_machine[2] = True
-    # 服务器的ip和端口
-    server = WebsocketServer(host='192.41.233.54', port=12345)  # GENI IP: 172.17.4.2
-    lb_thread = threading.Thread(target=load_balance)
+    server = WebsocketServer(host='192.41.233.54', port=12345)  # create websocket
+    lb_thread = threading.Thread(target=load_balance)  # start load_balance
     server.set_fn_message_received(message_received)
     lb_thread.start()
-    server.run_forever()
+    server.run_forever()  # Run websocket server
     print("ok")
